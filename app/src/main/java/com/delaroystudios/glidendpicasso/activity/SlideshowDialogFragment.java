39cd9b388a4a1cb4beaeca9a5f1adcc2b1eda8ca
Delaroy Studios <bamoyk@yahoo.com>
2018-02-06 06:59:51 +0100

    Initial commit

---
+++ app/src/main/java/com/delaroystudios/glidendpicasso/activity/SlideshowDialogFragment.java
@@ -0,0 +1,215 @@
+package com.delaroystudios.glidendpicasso.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.delaroystudios.glidendpicasso.R;
+import com.delaroystudios.glidendpicasso.model.Image;
+
+
+public class SlideshowDialogFragment extends DialogFragment {
+    private String TAG = SlideshowDialogFragment.class.getSimpleName();
+    private ArrayList<Image> images;
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private TextView lblCount, lblTitle, lblDate;
+    private int selectedPosition = 0;
+    String setImgLink = "";
+    ImageView share, download;
+
+    static SlideshowDialogFragment newInstance() {
+        SlideshowDialogFragment f = new SlideshowDialogFragment();
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_image_slider, container, false);
+        viewPager = (ViewPager) v.findViewById(R.id.viewpager);
+        lblCount = (TextView) v.findViewById(R.id.lbl_count);
+        lblTitle = (TextView) v.findViewById(R.id.title);
+        lblDate = (TextView) v.findViewById(R.id.date);
+
+        share = (ImageView) v.findViewById(R.id.share);
+        download = (ImageView) v.findViewById(R.id.download);
+
+        final ImageView imageViewPreview = (ImageView) v.findViewById(R.id.image_preview);
+
+        images = (ArrayList<Image>) getArguments().getSerializable("images");
+        selectedPosition = getArguments().getInt("position");
+
+        Log.e(TAG, "position: " + selectedPosition);
+        Log.e(TAG, "images size: " + images.size());
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        setCurrentItem(selectedPosition);
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareItem(setImgLink);
+            }
+        });
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = setImgLink;
+            }
+        });
+
+        return v;
+    }
+
+    public void shareItem(String url) {
+        Picasso.with(getContext()).load(url).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("image/*");
+                i.putExtra(Intent.EXTRA_STREAM, getLocalBitmapUri(bitmap));
+                startActivity(Intent.createChooser(i, "Share Image"));
+            }
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) { }
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) { }
+        });
+    }
+
+    public Uri getLocalBitmapUri(Bitmap bmp) {
+        Uri bmpUri = null;
+        try {
+            File file =  new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
+
+
+    private void setCurrentItem(int position) {
+        viewPager.setCurrentItem(position, false);
+        displayMetaInfo(selectedPosition);
+    }
+
+    //	page change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            displayMetaInfo(position);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    private void displayMetaInfo(int position) {
+        lblCount.setText((position + 1) + " of " + images.size());
+
+        Image image = images.get(position);
+        lblTitle.setText(image.getName());
+        setImgLink = image.getLarge().toString();
+        lblDate.setText(image.getTimestamp());
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+    }
+
+    //	adapter
+    public class MyViewPagerAdapter extends PagerAdapter {
+
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(R.layout.image_fullscreen_preview, container, false);
+
+            ImageView imageViewPreview = (ImageView) view.findViewById(R.id.image_preview);
+
+            Image image = images.get(position);
+
+            Glide.with(getActivity()).load(image.getLarge())
+                    .thumbnail(0.5f)
+                    .crossFade()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(imageViewPreview);
+
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == ((View) obj);
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+
+}
